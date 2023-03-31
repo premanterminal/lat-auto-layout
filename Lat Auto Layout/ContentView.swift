26a4e679dea49b6fd1204b84e5ba7aad3279c6cf
siti <siti@sitis-MacBook-Pro.local>
2023-03-31 09:34:30 +0700

    first commit

---
+++ Lat Auto Layout/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Lat Auto Layout
+//
+//  Created by siti on 28/03/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Ciye Belajar Swift")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Lat Auto Layout/Lat_Auto_LayoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lat_Auto_LayoutApp.swift
+//  Lat Auto Layout
+//
+//  Created by siti on 28/03/23.
+//
+
+import SwiftUI
+
+@main
+struct Lat_Auto_LayoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
